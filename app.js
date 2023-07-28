67ba652d5f621c5fc5c59caaec5852f9a2295933
rkrishirajkukreja <140739847+rkrishirajkukreja@users.noreply.github.com>
2023-07-28 17:43:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,81 +1,63 @@
+// server.js
 const express = require('express');
-const { createServer } = require('http');
+const http = require('http');
+const fs = require('fs');
+
 const app = express();
-const axios = require('axios');
+const server = http.createServer(app);
 
-// An in-memory variable to store the data
-let eventData = null;
-let eventSent = false; // To track if the event has been sent
+app.use(express.json()); // Parse JSON body in requests
 
-// Serve the index.html file
+// Serve the HTML page with the "Send Data to Client" button
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
-// SSE implementation
-app.get('/events2', (req, res) => {
-  res.setHeader('Content-Type', 'text/event-stream');
-  res.setHeader('Cache-Control', 'no-cache');
-  res.setHeader('Connection', 'keep-alive');
-
-  if (!eventSent) {
-    // Send the event data to the client
-    if (eventData) {
-      res.write(`data: ${eventData}\n\n`);
-    }
-    eventSent = true;
-
-    // End the SSE connection after sending the data
-    res.end();
-  }
-});
-
-// Endpoint to handle button click and update the data
-app.post('/send-event', (req, res) => {
-  // Generate the message to send to the client
-  const currentTimeStamp = new Date().toISOString();
-  eventData = `Hello From Server (Button Click) - ${currentTimeStamp}`;
-
-  // Send the response to the client
-  res.sendStatus(200);
-  res.end(); // End the response after sending the response data
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  fs.createReadStream('index.html').pipe(res);
 });
 
 const clients = new Map();
-let clientIdCounter = 1;
 
-app.get('/events', (req, res) => {
-  // Set headers for Server-Sent Events
-  res.setHeader('Content-Type', 'text/event-stream');
-  res.setHeader('Cache-Control', 'no-cache');
-  res.setHeader('Connection', 'keep-alive');
-
-  // Generate a unique ID for the client
-  const clientId = clientIdCounter++;
+// SSE endpoint for the clients to connect
+app.get('/sse/:clientId', (req, res) => {
+  const clientId = req.params.clientId;
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive',
+  });
 
   // Store the response object to send data later
   clients.set(clientId, res);
 
-  // Send initial data to the client
-  res.write(`data: Initial Data from Server\n\n`);
-
-  // Remove client from the list when the connection is closed
+  // Close the SSE connection when the client disconnects
   req.on('close', () => {
     clients.delete(clientId);
+    console.log(`Client ${clientId} disconnected.`);
   });
 });
 
-app.post('/send', express.json(), (req, res) => {
-  const { firstName, lastName, mobile, appointmenttime, operatory } = req.body;
-  const message = `Hello From Server - First Name: ${firstName}, Last Name: ${lastName}, Mobile: ${mobile}, ApptDateTime: ${appointmenttime}, Operatory: ${operatory} \n`;
-  clients.forEach((client) => {
-    client.write(`data: ${message}\n\n`);
-  });
-  res.status(200).send('Data sent to clients.');
+// Function to send data to a specific client
+function sendDataToClient(clientId, data) {
+  const clientResponse = clients.get(clientId);
+  if (clientResponse) {
+    clientResponse.write(`data: ${JSON.stringify(data)}\n\n`);
+  }
+}
+
+// Route to handle sending data to the selected client
+app.post('/send-data-to-client', (req, res) => {
+  const selectedClient = req.query.client;
+  if (!selectedClient) {
+    res.status(400).send('Client not specified.');
+    return;
+  }
+
+  const data = req.body;
+  sendDataToClient(selectedClient, data);
+
+  res.send('Data sent to client.');
 });
 
-// Start the server
-const server = createServer(app);
-server.listen(3000, () => {
-  console.log('Server listening on port 3000');
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
